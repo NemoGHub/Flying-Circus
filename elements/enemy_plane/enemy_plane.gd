0444511a0783317d264c669cf351f1e23b4a2014
Александр Сминов ПрИ-202 <smirnovalexandr234@gmail.com>
2025-04-13 20:05:59 +0500

    Fokker DrI

--- elements/enemy_plane/enemy_plane.gd
+++ elements/enemy_plane/enemy_plane.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 1
 const turnRate = 300.0
 var HEALTH = 1
+const ramDmage = 10
 
 const bullet_scene = preload("res://elements/Bullet/bullet_2d.tscn")
 var texture = preload("res://assets/plane.png")
@@ -14,7 +14,13 @@ func _physics_process(delta: float) :
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	delta = delta + SPEED
-	move_and_collide(Vector2.DOWN * SPEED * delta)
+	var collision = move_and_collide(Vector2.DOWN * SPEED * delta)
+	if collision:
+		var collider = collision.get_collider()
+		if collider and collider.has_method("shot"):
+			print('RAM_EM!')
+			collider.shot(ramDmage)
+			queue_free()
 	
 func mg_fire():
 	var shell = bullet_scene.instantiate()	
--- elements/plane/plane.gd
+++ elements/plane/plane.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-
+var HEALTH = 10
+const ramDmage = 10
 const SPEED = 110
 const turnRate = 300.0
 const fireRate = 0.12
@@ -9,9 +10,12 @@ var time_since_last_shot = 0.0
 
 
 const bullet_scene = preload("res://elements/Bullet/bullet_2d.tscn")
-var texture = preload("res://assets/plane.png")
-var texture_to_left = preload("res://assets/plane_to_left.png")
-var texture_to_right = preload("res://assets/plane_to_right.png")
+#var texture = preload("res://assets/plane.png")
+#var texture_to_left = preload("res://assets/plane_to_left.png")
+#var texture_to_right = preload("res://assets/plane_to_right.png")
+var texture = preload("res://assets/FokkerDrI/fokker_dr1.png")
+var texture_to_left = preload("res://assets/FokkerDrI/fokker_dr1_to_left.png")
+var texture_to_right = preload("res://assets/FokkerDrI/fokker_dr1_to_right.png")
 
 func _physics_process(delta: float) :
 	# Get the input direction and handle the movement/deceleration.
@@ -35,14 +39,27 @@ func _physics_process(delta: float) :
 #global_position.x = direction * SPEED * delta
 	move_and_slide()
 	
+	
 func mg_fire():
 	animate_fire()
 	AMMO -= 1
 	var shell = bullet_scene.instantiate()	
-	shell.global_position = global_position + Vector2(-2, -40)
+	shell.global_position = global_position + Vector2(0, -50)
 	add_child(shell)
 
 	
 func animate_fire():
 	$Sprite2D/tratata.play("tratata")
+	
+func shot(damage):
+	HEALTH -= damage
+	if HEALTH < 0:
+		print('Hit')
+		shot_down()
+		
+func shot_down():
+	print('Shot down!')
+	queue_free()
+	
+	
 	
